007daba3d750427bb1d3b89a07b15a162cf62dad
Michel Galan <michelgalan90@gmail.com>
2021-07-02 10:52:26 -0400

    sumando

---
+++ Suma/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int valor1, valor2, suma;
+
+    valor1 = 20;
+    valor2 = 12;
+
+    suma = valor1 + valor2;
+
+    printf("La suma de %i y %i es %i \n", valor1, valor2, suma);
+
+    return 0;
+}
