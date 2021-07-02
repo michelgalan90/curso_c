68067c57b3db7c4c7e470fc2ba1b6a894e819c69
Michel Galan <michelgalan90@gmail.com>
2021-07-02 10:07:04 -0400

    hola Mundo

---
+++ holamundo/facil.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*codigo hecho por michel en el n del 2021 reservados*/
+
+int main(){  
+/*Esto es un comentario de varias lineas
+*/
+	//Imprime hola mundo con salto de linea
+	printf("Hola mundo\n");
+	printf("Hola mundo");
+	return 0;
+}
---
+++ preprocesadores/preprocesadores.c
@@ -0,0 +1,22 @@
+//Directivas de Preprocesador y Variables
+
+#include <stdio.h> // Libreria
+/*
+#include <string.h>
+#include <math.h>
+#include <stdlib.h> 
+*/
+
+#define PI 3.14.16; //Macro
+
+int y = 5; //Variable global
+
+int main(){
+	int x = 10; // Variable local
+	
+	int suma = 0;
+	
+	return 0;
+}
+
+
