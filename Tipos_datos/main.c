b84e59281f8aeb89f81aeee6b2df2e4b694b2ca8
Michel Galan <michelgalan90@gmail.com>
2021-07-02 13:26:40 -0400

    Tipo de datos

---
+++ Tipos_datos/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char characterName[] = "Michel";
+    char caracter = 'h';
+    printf("%c\n", caracter);
+
+    char
+    int numero = 5;
+    numero = 8;
+    int rgbColor = 0xFFEF0D;
+
+    double salario = 25600.23;
+
+    printf("Mi numero favorito es: %d\n", rgbColor);
+
+    float a = 34.56;
+
+    printf("%f\n", a);
+
+    printf("    /|\n");
+    printf("   / |\n");
+    printf("  /  |\n");
+    printf(" /   |\n");
+    printf("/____|\n");
+
+    return 0;
+}
