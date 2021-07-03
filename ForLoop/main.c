9cda80ca533d2e859939a2107a7048a34f8a63d1
Michel Galan <michelgalan90@gmail.com>
2021-07-02 20:16:49 -0400

    ForLoop

---
+++ ForLoop/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int i = 0;
+
+    int randomNumbers[] = {10, 56, 48, 72, 53, 59, 78};
+
+    for(i; i<=6; i++){
+        printf("El numero es: %d\n", randomNumbers[i]);
+    }
+
+    return 0;
+}
